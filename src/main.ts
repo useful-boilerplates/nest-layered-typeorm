0ab3311a9cc32d3b84b8a846c61e6c723953b1f3
Mevlüt Kural <mevlutkural35@gmail.com>
2026-01-02 22:13:54 +0300

    feat: Add CORS and API throttling configuration and integration.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { LoggerService } from './shared/logger';
+import { setupCors } from './shared/bootstrap/cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = app.get(LoggerService);
 
   app.useLogger(logger);
+
+  setupCors(app, logger);
 
   await app.listen(process.env.PORT ?? 3000);
 }
