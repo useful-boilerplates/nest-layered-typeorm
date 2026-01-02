3b3672699a8ab2aa7b7844bc7375679f4b2d956a
Mevlüt Kural <mevlutkural35@gmail.com>
2026-01-02 21:37:19 +0300

    feat: Introduce NestJS Config Module for environment-based application configuration.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { appConfig } from './shared/config/app.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [appConfig],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
